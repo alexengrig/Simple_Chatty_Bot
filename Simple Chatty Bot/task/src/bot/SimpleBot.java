197bc4086647c08456660319e40d69b68c4ae616
Grig Alex <alexengrigdev@gmail.com>
2019-12-12 22:12:56 +0300

    Add 2/4 solution

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,24 +1,32 @@
 package bot;
 
-import java.time.Year;
+import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
-        Bot bot = new Bot("Aid");
-        bot.greeting();
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    static class Bot {
-        private String name;
-        private Year birthYear;
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
 
-        public Bot(String name) {
-            this.name = name;
-            this.birthYear = Year.now();
+        String name = scanner.nextLine();
+
+        System.out.printf("What a great name you have, %s!\n", name);
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int three = scanner.nextInt();
+        int five = scanner.nextInt();
+        int seven = scanner.nextInt();
+        int age = 0;
+        for (int i = 0; i <= 12; i++) {
+            age = i * 7 + seven;
+            if (age % 3 == three && age % 5 == five) {
+                break;
+            }
         }
 
-        public void greeting() {
-            System.out.printf("Hello! My name is %s\nI was created in %d\n", name, birthYear.getValue());
-        }
+        System.out.printf("Your age is %d; that's a good time to start programming!", age);
     }
 }
